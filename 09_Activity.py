464faa34cf08c3060a029ade8148e5e5598bc958
FranciscoKaleb <116531037+FranciscoKaleb@users.noreply.github.com>
2026-01-27 07:44:06 +0800

    Add files via upload

---
+++ 09_Activity.py
@@ -0,0 +1,36 @@
+
+# Deductions:
+# [1] GSIS 
+    # 9% of monthly salary
+# [2] PhilHealth 
+    # For monthly income ₱10,000 and below, total contribution is ₱250; 
+    # for ₱10,000.01 to ₱99,999.99 ->  2.5% of monthly salary;        
+    # for ₱100,000 and above, it is ₱5,000.
+# [3] Pag-IBIG
+    # 1500 below monthly salary -> 1% of monthly Salary
+    # 1501 - 10000 -> 2% of monthly Salary
+    # 10000 and above -> fixed 200
+# [4] Withholding tax
+    # 0 - 20832 -> 0%
+    # 20833 - above -> 20% of the excess over 20832
+    # theres other condition for higher salary but for this activity we will just use this
+    
+
+# Create a program that calculates the net pay of a government employee after deductions.
+# use given above deduction rules
+
+print('Enter your monthly wage:')
+monthly_wage = float(input())
+# type your code here
+
+
+print('GSIS Deduction: ')
+print('PhilHealth Deduction: ')
+print('Pag-IBIG Deduction: ')
+print('Withholding Tax Deduction: ')
+print('Net pay: ')
+
+
+
+
+
