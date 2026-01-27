decfcda7dbdc695a8b13639711f9d7513ae0c88b
FranciscoKaleb <116531037+FranciscoKaleb@users.noreply.github.com>
2026-01-28 07:22:54 +0800

    Add files via upload

---
+++ 09_Activity_2.py
@@ -0,0 +1,28 @@
+
+
+# Calculate the total commission earned by Agent, unit manager, branch head in 5 years-
+# -if client pays 100,000 per year
+
+# Year 1	(Selling agent 25%, Unit Manager 15%, Branch Head 10%)	
+# Year 2	(Selling agent 8%, Unit Manager 5%, Branch Head 2%)	
+# Year 3	(Selling agent 5%, Unit Manager 4%, Branch Head 1%)	
+# Year 4	(Selling agent 3%, Unit Manager 2%, Branch Head 0%)	
+# Year 5	(Selling agent 2%, Unit Manager 1% Branch Head 0%)	
+
+# no if else involve in this problem
+
+print('-----------------[ 5 year Insurance Commission Calculation]------------------')
+amount = 100000
+agent_com = 0
+unit_m_com = 0
+branch_h_com = 0
+
+
+
+print(f'5 years commission breakdown:')
+print(f'Agent: {agent_com}')
+print(f'Unit manager: {unit_m_com}')
+print(f'Branch Head: {branch_h_com}')
+
+
+
